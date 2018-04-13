89443389066a1c3db047465b02726cb766aa27eb
GenialX <admin@ihuxu.com>
2018-04-13 12:10:05 +0800

    :feature: get location in background

--- app/src/main/java/com/ihuxu/hestia_android/MainActivity.java
+++ app/src/main/java/com/ihuxu/hestia_android/MainActivity.java
@@ -1,23 +1,13 @@
 package com.ihuxu.hestia_android;
 
-import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -27,8 +17,6 @@ import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
-import com.ihuxu.hestia_android.libs.Location;
-import com.ihuxu.hestia_android.libs.job.JobSchedulerService;
 import com.ihuxu.hestia_android.libs.server.ServerThread;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
     public AMapLocationClient mLocationClient = null;
     public AMapLocationListener mLocationListener = null;
+
+    private static final String NOTIFICATION_CHANNEL_NAME = "BackgroundLocation";
+    private NotificationManager notificationManager = null;
+    boolean isCreateChannel = false;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -90,8 +83,44 @@ public class MainActivity extends AppCompatActivity {
 
         if(null != mLocationClient){
             mLocationClient.setLocationOption(mLocationOption);
+            mLocationClient.enableBackgroundLocation(2001, buildNotification());
             mLocationClient.stopLocation();
             mLocationClient.startLocation();
         }
+    }
+
+    private Notification buildNotification() {
+        Notification.Builder builder = null;
+        Notification notification = null;
+        if(android.os.Build.VERSION.SDK_INT >= 26) {
+            //Android O上对Notification进行了修改，如果设置的targetSDKVersion>=26建议使用此种方式创建通知栏
+            if (null == notificationManager) {
+                notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            String channelId = getPackageName();
+            if(!isCreateChannel) {
+                NotificationChannel notificationChannel = new NotificationChannel(channelId,
+                        NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+                notificationChannel.enableLights(true);//是否在桌面icon右上角展示小圆点
+                notificationChannel.setLightColor(Color.BLUE); //小圆点颜色
+                notificationChannel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
+                notificationManager.createNotificationChannel(notificationChannel);
+                isCreateChannel = true;
+            }
+            builder = new Notification.Builder(getApplicationContext(), channelId);
+        } else {
+            builder = new Notification.Builder(getApplicationContext());
+        }
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Hestia")
+                .setContentText("正在后台运行")
+                .setWhen(System.currentTimeMillis());
+
+        if (android.os.Build.VERSION.SDK_INT >= 16) {
+            notification = builder.build();
+        } else {
+            return builder.getNotification();
+        }
+        return notification;
     }
 }
